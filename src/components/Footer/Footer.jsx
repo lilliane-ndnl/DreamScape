40f64ecde493d57d75562a725878adc3b7e37b9f
Lilliane Nguyen <135808754+lilliane-nguyen@users.noreply.github.com>
2025-03-09 10:21:00 +0700

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Lovingly made by <a href="https://www.linkedin.com/in/lilliane-nguyen/" target="_blank" rel="noopener noreferrer">Lilliane</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer; 
