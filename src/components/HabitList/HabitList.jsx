f83a3b721e12c2ca4937bd65d749625c0d4945ad
Lilliane Nguyen <135808754+lilliane-nguyen@users.noreply.github.com>
2025-03-09 10:28:07 +0700

    Create HabitList.jsx

---
+++ src/components/HabitList/HabitList.jsx
@@ -0,0 +1,25 @@
+.habitItem {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    border-bottom: 1px solid #eee;
+  }
+  
+  .checkbox {
+    margin-right: 10px;
+  }
+  
+  .habitName {
+    flex-grow: 1; /* Take up remaining space */
+    font-size: 16px;
+  }
+  .deleteButton{
+      background-color: #dc3545;
+      color: white;
+      border: none;
+      border-radius: 4px;
+      padding: 5px 10px;
+      cursor: pointer;
+      font-size: 14px;
+      margin-left: 5px;
+  }
